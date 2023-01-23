50fbbd08f7ba68113154f53998e755195c3ae330
valentinLW <valentinhobusch+lw@gmail.com>
2023-01-23 14:44:12 +0100

    input fields and state list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
 
 export default function Home() {
+  const [cards, setCards] = useState([
+    {'front': 'tree', 'back': 'árvore'},
+    {'front': 'ball', 'back': 'bola'}
+  ]);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className='md:container mx-auto'>
+      <h1 className='text-3xl font-bold underline'>
+        add card
+      </h1>
+      <form>
+        <div className='grid gap-6 mb-6 md:grid-cols-2'>
+          <div>
+            <label htmlFor='front' className='mb-2 text-sm text-gray-900'>Front</label>
+            <input type='text' id='front' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5' placeholder='tree'/>
+          </div>
+          <div>
+            <label htmlFor='back' className='mb-2 text-sm text-gray-900'>Back</label>
+            <input type='text' id='back' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5' placeholder='árvore'/>
+          </div>
+        </div>
+      </form>
+
+      <div>
+        {cards.map((card) => (
+          <div key={card.front} className='mb-2 flex flex-row bg-slate-100 border rounded-md p-3 divide-x'>
+            <div className='basis-1/2 text-center'>{card.front}</div>
+            <div className='basis-1/2 text-center'>{card.back}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
